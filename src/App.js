0423f6341d8a53c441a6aed2d4b02a707ace584e
Ribin <ribinroy4@gmail.com>
2020-11-10 00:39:13 +0530

    Quick Fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import logo from './logo.svg';
 import './App.css';
 import firebase from './api/Firebase';
 
 function App() {
-    // debugger;
     console.log(firebase);
-    return <div className='App'>Hell yeah!</div>;
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
