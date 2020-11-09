279e7d2a58e92809e7870bb0bccaf2290e53a15d
Ribin <ribinroy4@gmail.com>
2020-11-10 00:51:12 +0530

    CI CD Fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import firebase from './api/Firebase';
 
 function App() {
     console.log(firebase);
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+    return <div className='App'>Hell yeah!</div>;
 }
 
 export default App;
